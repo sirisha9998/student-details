2df3868e472110ee53b84b0f3fcaefff6e15d0d5
sirisha9998 <42994962+sirisha9998@users.noreply.github.com>
2018-09-05 13:27:31 +0530

    Add files via upload

---
+++ MSA/Studentdetails.c
@@ -0,0 +1,15 @@
+int main()
+{
+	char studentname[70],branch[70];
+	int age[70],year[70],semester[70],previousscore[70];
+	printf("enter student name:");
+	scanf("%s",studentname[70]);
+	printf("enter age of the student");
+	scanf("%d",&age[70]);
+    printf("enter student branch");
+    scanf("%s",branch[70]);
+    printf("enter student's present year");
+    scanf("%d",&year[70]);
+    printf("enter students present semester");
+    scanf("%d",&semester[70]);
+}
